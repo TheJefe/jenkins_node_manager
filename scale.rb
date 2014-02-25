ab9d26120913c19717af7c5674335942533f33e5
Jeff Koenig <jkoenig@thinkthroughmath.com>
2014-02-25 16:15:15 -0500

    refactor variables

--- scale.rb
+++ scale.rb
@@ -1,104 +1,105 @@
+#!/usr/bin/env ruby
+
 require 'net/http'
 require 'json'
 require 'uri'
 
-@hostname=ENV['JENKINS_HOSTNAME']
-@port=ENV['JENKINS_PORT']
-@username=ENV['JENKINS_USERNAME']
-@api_key=ENV['JENKINS_API_KEY']
+HOSTNAME=ENV['JENKINS_HOSTNAME']
+PORT=ENV['JENKINS_PORT']
+USERNAME=ENV['JENKINS_USERNAME']
+API_KEY=ENV['JENKINS_API_KEY']
 
-@MIN_NODES=ENV['MIN_NODES']
-@MAX_NODES=ENV['MAX_NODES']
+MIN_NODES=ENV['MIN_NODES']
+MAX_NODES=ENV['MAX_NODES']
 
-@main_url="http://#{@hostname}:#{@port}"
+MAIN_URL="http://#{HOSTNAME}:#{PORT}"
 
-@node_list_endpoint = "/computer/api/json" ##endpoint to get a list of nodes
-@node_add_endpoint = "/job/Node-add/buildWithParameters?NUM_NODES="
-@node_delete_endpoint = "/job/Node-delete/build??delay=0sec"
-@build_queue_endpoint = "/queue/api/json"
+NODE_LIST_ENDPOINT = "/computer/api/json" ##endpoint to get a list of nodes
+NODE_ADD_ENDPOINT = "/job/Node-add/buildWithParameters?NUM_NODES="
+NODE_DELETE_ENDPOINT = "/job/Node-delete/build??delay=0sec"
+BUILD_QUEUE_ENDPOINT = "/queue/api/json"
 
 # this method gets data from a specified endpoint and returns it as a string
 def http_get(endpoint)
-        uri= URI.parse "#{@main_url}#{endpoint}"
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request.basic_auth(@username, @api_key)
-        response = http.request(request)
-        response.body
+  uri= URI.parse "#{MAIN_URL}#{endpoint}"
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request.basic_auth(USERNAME, API_KEY)
+  response = http.request(request)
+  response.body
 end
 
 # this method will find the first  node name that is currently "offline" and return it.
 def find_available_node
-        node_name=nil
-        nodes = JSON.parse http_get(@node_list_endpoint)
-        nodes["computer"].each do |i|
-                if i["offline"]
-                        node_name=i["displayName"]
-                        break
-                end
-        end
+  node_name=nil
+  nodes = JSON.parse http_get(NODE_LIST_ENDPOINT)
+  nodes["computer"].each do |i|
+    if i["offline"]
+      node_name=i["displayName"]
+      break
+    end
+  end
 
-        return node_name
+  return node_name
 end
 
 # checks to make sure the required environment variables are set
 def check_environment
-  if @hostname == nil || @port == nil || @username == nil || @api_key == nil || @MIN_NODES == nil || @MAX_NODES == nil
+  if HOSTNAME == nil || PORT == nil || USERNAME == nil || API_KEY == nil || MIN_NODES == nil || MAX_NODES == nil
     raise "environment variables not set. Please check that you have the following set...\
 \nJENKINS_HOSTNAME\nJENKINS_PORT\nJENKINS_USERNAME\nJENKINS_API_KEY\nMIN_NODES\nMAX_NODES"
   end
 end
 
 def add_nodes(num)
-                puts "adding #{num} nodes"
-        num.times do
-                http_get("#{@node_add_endpoint}#{num}")
-        end
+  puts "adding #{num} nodes"
+  num.times do
+    http_get("#{NODE_ADD_ENDPOINT}#{num}")
+  end
 end
 
 def delete_nodes(num)
-                puts "deleting #{num} nodes"
-        num.times do
-                http_get(@node_delete_endpoint)
-                sleep 5
-        end
+  puts "deleting #{num} nodes"
+  num.times do
+    http_get(NODE_DELETE_ENDPOINT)
+    sleep 5
+  end
 end
 
 def scale_nodes()
-        node_info   = JSON.parse( http_get(@node_list_endpoint) )
-        build_queue = JSON.parse( http_get(@build_queue_endpoint) )
-        num_queued = build_queue["items"].count
-        total_executors = node_info["totalExecutors"]
-        busy_nodes = node_info["busyExecutors"]
-        difference = total_executors - busy_nodes
-        scale_by = 0
+  node_info   = JSON.parse( http_get(NODE_LIST_ENDPOINT) )
+  build_queue = JSON.parse( http_get(BUILD_QUEUE_ENDPOINT) )
+  num_queued = build_queue["items"].count
+  total_executors = node_info["totalExecutors"]
+  busy_nodes = node_info["busyExecutors"]
+  difference = total_executors - busy_nodes
+  scale_by = 0
 
-        puts "total_executors: #{total_executors}"
-        puts "busy_nodes: #{busy_nodes}"
-        puts "difference #{difference}"
-        puts "queued jobs: #{num_queued}"
+  puts "total_executors: #{total_executors}"
+  puts "busy_nodes: #{busy_nodes}"
+  puts "difference #{difference}"
+  puts "queued jobs: #{num_queued}"
 
-        if num_queued > 0
-                # don't go over the max count
-                scale_by = (total_executors + num_queued) <= @MAX_NODES ? num_queued : @MAX_NODES - total_executors
-        else
-                if ( (total_executors - difference) >= @MIN_NODES )
-                        scale_by = -1 * difference
-                else
-                        scale_by = -1 * ( total_executors - @MIN_NODES)
-                end
+  if num_queued > 0
+    # don't go over the max count
+    scale_by = (total_executors + num_queued) <= MAX_NODES ? num_queued : MAX_NODES - total_executors
+  else
+    if ( (total_executors - difference) >= MIN_NODES )
+      scale_by = -1 * difference
+    else
+      scale_by = -1 * ( total_executors - MIN_NODES)
+    end
 
-        end
+  end
 
-        if scale_by > 0
-                add_nodes scale_by
-        else
-                delete_nodes(-1 * scale_by)
-        end
+  if scale_by > 0
+    add_nodes scale_by
+  else
+    delete_nodes(-1 * scale_by)
+  end
 end
 
-#### MAIN ####
-
+#### MAIN
 check_environment
-scale_nodes()
+scale_nodes
 
