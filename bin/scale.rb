e2421cf1f61eaf38c8e85e3f11e23d4e05fe8c64
Jeff Koenig <jkoenig@thinkthroughmath.com>
2014-03-18 09:45:26 -0400

    setup a basic directory structure to share a jenkins library

--- bin/scale.rb
+++ bin/scale.rb
@@ -3,45 +3,10 @@
 require 'net/http'
 require 'json'
 require 'uri'
-
-HOSTNAME=ENV['JENKINS_HOSTNAME']
-PORT=ENV['JENKINS_PORT']
-USERNAME=ENV['JENKINS_USERNAME']
-API_KEY=ENV['JENKINS_API_KEY']
+require '../lib/jenkins.rb'
 
 MIN_NODES=ENV['MIN_NODES'].to_i
 MAX_NODES=ENV['MAX_NODES'].to_i
-
-MAIN_URL="http://#{HOSTNAME}:#{PORT}"
-
-NODE_LIST_ENDPOINT = "/computer/api/json" ##endpoint to get a list of nodes
-NODE_ADD_ENDPOINT = "/job/Node-add/buildWithParameters?NUM_NODES="
-NODE_DELETE_ENDPOINT = "/job/Node-delete/build??delay=0sec"
-BUILD_QUEUE_ENDPOINT = "/queue/api/json"
-
-# this method gets data from a specified endpoint and returns it as a string
-def http_get(endpoint)
-  uri= URI.parse "#{MAIN_URL}#{endpoint}"
-  http = Net::HTTP.new(uri.host, uri.port)
-  request = Net::HTTP::Get.new(uri.request_uri)
-  request.basic_auth(USERNAME, API_KEY)
-  response = http.request(request)
-  response.body
-end
-
-# this method will find the first  node name that is currently "offline" and return it.
-def find_available_node
-  node_name=nil
-  nodes = JSON.parse http_get(NODE_LIST_ENDPOINT)
-  nodes["computer"].each do |i|
-    if i["offline"]
-      node_name=i["displayName"]
-      break
-    end
-  end
-
-  return node_name
-end
 
 # checks to make sure the required environment variables are set
 def check_environment
@@ -56,24 +21,9 @@ def check_environment
 
 end
 
-def add_nodes(num)
-  puts "adding #{num} nodes"
-  num.times do
-    http_get("#{NODE_ADD_ENDPOINT}#{num}")
-  end
-end
-
-def delete_nodes(num)
-  puts "deleting #{num} nodes"
-  num.times do
-    http_get(NODE_DELETE_ENDPOINT)
-    sleep 5
-  end
-end
-
 def scale_nodes()
-  node_info   = JSON.parse( http_get(NODE_LIST_ENDPOINT) )
-  build_queue = JSON.parse( http_get(BUILD_QUEUE_ENDPOINT) )
+  node_info   = JSON.parse( Jenkins.http_get(NODE_LIST_ENDPOINT) )
+  build_queue = JSON.parse( Jenkins.http_get(BUILD_QUEUE_ENDPOINT) )
   num_queued = build_queue["items"].count
   master_node = node_info["computer"].select {|x| x['displayName'] == "master"}.first
   master_executors = master_node["numExecutors"]
@@ -104,10 +54,10 @@ def scale_nodes()
 
   if scale_by > 0
     puts "scale up by #{scale_by}"
-    add_nodes scale_by unless ARGV[0] =~ /pretend/
+    Jenkins.add_nodes scale_by unless ARGV[0] =~ /pretend/
   else
     puts "scale down by #{ -1 *scale_by}"
-    delete_nodes(-1 * scale_by) unless ARGV[0] =~ /pretend/
+    Jenkins.delete_nodes(-1 * scale_by) unless ARGV[0] =~ /pretend/
   end
 end
 
---
+++ lib/jenkins.rb
@@ -0,0 +1,51 @@
+HOSTNAME=ENV['JENKINS_HOSTNAME']
+PORT=ENV['JENKINS_PORT']
+USERNAME=ENV['JENKINS_USERNAME']
+API_KEY=ENV['JENKINS_API_KEY']
+MAIN_URL="http://#{HOSTNAME}:#{PORT}"
+NODE_LIST_ENDPOINT = "/computer/api/json" ##endpoint to get a list of nodes
+NODE_ADD_ENDPOINT = "/job/Node-add/buildWithParameters?NUM_NODES="
+NODE_DELETE_ENDPOINT = "/job/Node-delete/build??delay=0sec"
+BUILD_QUEUE_ENDPOINT = "/queue/api/json"
+
+class Jenkins
+
+  # this method gets data from a specified endpoint and returns it as a string
+  def self.http_get(endpoint)
+    uri= URI.parse "#{MAIN_URL}#{endpoint}"
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.basic_auth(USERNAME, API_KEY)
+    response = http.request(request)
+    response.body
+  end
+
+  # this method will find the first  node name that is currently "offline" and return it.
+  def self.find_available_node
+    node_name=nil
+    nodes = JSON.parse http_get(NODE_LIST_ENDPOINT)
+    nodes["computer"].each do |i|
+      if i["offline"]
+        node_name=i["displayName"]
+        break
+      end
+    end
+
+    return node_name
+  end
+
+  def self.add_nodes(num)
+    puts "adding #{num} nodes"
+    num.times do
+      http_get("#{NODE_ADD_ENDPOINT}#{num}")
+    end
+  end
+
+  def self.delete_nodes(num)
+    puts "deleting #{num} nodes"
+    num.times do
+      http_get(NODE_DELETE_ENDPOINT)
+      sleep 5
+    end
+  end
+end
